cf641757af655647185a09fd790d1eba43332ad6
SoyBison <coeneedell@gmail.com>
2022-02-08 16:20:55 -0500

    finished basic cdx filter

--- cdx.py
+++ cdx.py
@@ -2,8 +2,11 @@ import pandas as pd
 import json
 import requests
 import datetime
+from utils import intervals
+from tqdm import tqdm
 
 WAYBACK_FORMAT = '%Y%m%d%H%M%S'
+
 
 class WaybackCDX:
     def __init__(self):
@@ -21,21 +24,40 @@ class WaybackCDX:
         return df
 
     def download_period(self, url, period_start: datetime.datetime, period_end: datetime.datetime):
-        period_start_wbc = period_start.strftime(WAYBACK_FORMAT)
-        period_end_wbc = period_end.strftime(WAYBACK_FORMAT)
-        raw_json = requests.get(
-            self.cdx_format.replace('$URL', url).replace('$START', period_start_wbc).replace('$END', period_end_wbc))
+        cdx_req = self.cdx_format.replace('$URL', url)
+        if period_end is not None:
+            period_end_wbc = period_end.strftime(WAYBACK_FORMAT)
+            cdx_req = cdx_req.replace('$END', period_end_wbc)
+        else:
+            cdx_req = cdx_req.replace('&to=$END', '')
+        if period_start is not None:
+            period_start_wbc = period_start.strftime(WAYBACK_FORMAT)
+            cdx_req = cdx_req.replace('$START', period_start_wbc)
+        else:
+            cdx_req = cdx_req.replace('&from=$START', '')
+        raw_json = requests.get(cdx_req)
         listy_data = json.loads(raw_json.text)
         first_line = listy_data.pop(0)
         df = pd.DataFrame.from_records(listy_data, columns=first_line)
+        df['datetime'] = df['timestamp'].apply(lambda s: datetime.datetime.strptime(s, WAYBACK_FORMAT))
         return df
 
     def get_intervals(self, url, hrs=1, period_start=None, period_end=None):
-        df = self.download_all(url)
+        if period_start and period_end:
+            df = self.download_period(url, period_start, period_end)
+        else:
+            df = self.download_all(url)
+        reference_times = intervals(period_start, period_end, hrs=hrs)
+        storage_collection = df['datetime'].copy(deep=True)
+        target_times = set()
+        for time in tqdm(list(reference_times)):
+            storage_collection = storage_collection[storage_collection >= time]
+            target_times.add(min(storage_collection))
+        return df[df['datetime'].isin(target_times)]
+
 
 
 if __name__ == '__main__':
     scr = WaybackCDX()
-    print(scr.download_period('www.nytimes.com', datetime.datetime(2020, 1, 1), datetime.datetime(2020, 12, 31)).head())
-
-
+    print(scr.get_intervals('www.nytimes.com', hrs=1, period_start=datetime.datetime(2020, 1, 1),
+                            period_end=datetime.datetime(2020, 12, 31, 23, 59, 59)))
