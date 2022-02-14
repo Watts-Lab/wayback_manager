b07cfca5be470f21d15cd95cce3a739cc0c44b40
SoyBison <coeneedell@gmail.com>
2022-02-14 17:57:41 -0500

    Imported scrapers, changed dump location to NAS

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ if __name__ == '__main__':
     intervals = cdxer.get_intervals('www.nytimes.com', hrs=1, period_start=datetime.datetime(2020, 1, 1),
                                     period_end=datetime.datetime(2020, 12, 31, 23, 59, 59))
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
-    if not os.path.exists('data/nytimes'):
-        os.mkdir('data/nytimes')
+    if not os.path.exists('/home/coen/Remote/Data/Wayback'):
+        os.mkdir('/home/coen/Remote/Data/Wayback')
+    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes'):
+        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes')
 
     tqdm.write('Downloading data...')
 
     for timestamp in tqdm(intervals['timestamp']):
         article = scraper.scrape_article('www.nytimes.com', timestamp)
-        with open(f'data/nytimes/{timestamp}.pkl', 'wb') as f:
+        with open(f'/home/coen/Remote/Data/Wayback/nytimes/{timestamp}.pkl', 'wb') as f:
             pickle.dump(article, f)
