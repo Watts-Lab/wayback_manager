54e96805dfd5c9934e231bc7471134525760aefb
soybison <coeneedell@gmail.com>
2022-04-27 15:01:26 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import configparser
 import time
-from scrapers.wsj import WSJScraper
+from scrapers.nytimes import NYTimesScraper
 from cdx import WaybackCDX, WAYBACK_FORMAT
 import datetime
 import pickle
@@ -15,10 +15,10 @@ config.read(os.path.join(HERE, 'config.ini'))
 
 def get_request(timestamp, intervals):
     try:
-        r = requests.get(f'https://web.archive.org/web/{timestamp}/https://www.wsj.com/')
+        r = requests.get(f'https://web.archive.org/web/{timestamp}/https://www.nytimes.com/')
     except requests.exceptions.ConnectionError:
         time.sleep(60)
-        r = requests.get(f'https://web.archive.org/web/{timestamp}/https://www.wsj.com/')
+        r = requests.get(f'https://web.archive.org/web/{timestamp}/https://www.nytimes.com/')
     except requests.exceptions.TooManyRedirects:
         time.sleep(60)
         new_timestamp_i = int(intervals.set_index('timestamp').index.get_loc(timestamp)) + 1
@@ -29,38 +29,38 @@ def get_request(timestamp, intervals):
 
 if __name__ == '__main__':
     cdxer = WaybackCDX()
-    scraper = WSJScraper()
+    scraper = NYTimesScraper()
     tqdm.write('Acquiring CDX data')
-    intervals = cdxer.get_intervals('www.wsj.com', hrs=1, period_start=datetime.datetime(2015, 1, 1),
-                                    period_end=datetime.datetime(2019, 12, 31, 23, 59, 59))
+    intervals = cdxer.get_intervals('www.nytimes.com', hrs=1, period_start=datetime.datetime(2020, 1, 1),
+                                    period_end=datetime.datetime.now())
 
     if not os.path.exists('/home/coen/Remote/Data/Wayback'):
         os.mkdir('/home/coen/Remote/Data/Wayback')
-    if not os.path.exists('/home/coen/Remote/Data/Wayback/wsj'):
-        os.mkdir('/home/coen/Remote/Data/Wayback/wsj')
-    if not os.path.exists('/home/coen/Remote/Data/Wayback/wsj/raw'):
-        os.mkdir('/home/coen/Remote/Data/Wayback/wsj/raw')
-    if not os.path.exists('/home/coen/Remote/Data/Wayback/wsj/articles'):
-        os.mkdir('/home/coen/Remote/Data/Wayback/wsj/articles')
+    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes'):
+        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes')
+    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes/raw'):
+        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes/raw')
+    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes/articles'):
+        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes/articles')
 
     tqdm.write('Downloading data...')
 
     for timestamp in tqdm(intervals['timestamp'][intervals['is_target']]):
-        if os.path.exists(f'/home/coen/Remote/Data/Wayback/wsj/raw/{timestamp}.pkl'):
+        if os.path.exists(f'/home/coen/Remote/Data/Wayback/nytimes/raw/{timestamp}.pkl'):
             continue
         r = get_request(timestamp, intervals)
         html = r.text
         try:
             article_metadata = scraper.get_top_article_metadata(html)
-            with open(f'/home/coen/Remote/Data/Wayback/wsj/{timestamp}.pkl', 'wb') as f:
+            with open(f'/home/coen/Remote/Data/Wayback/nytimes/{timestamp}.pkl', 'wb') as f:
                 pickle.dump(article_metadata, f)
-            with open(f'/home/coen/Remote/Data/Wayback/wsj/raw/{timestamp}.pkl', 'wb') as f:
+            with open(f'/home/coen/Remote/Data/Wayback/nytimes/raw/{timestamp}.pkl', 'wb') as f:
                 pickle.dump(html, f)
             # for article in article_metadata:
             #     art_dump = scraper.extract(timestamp)
-            #     with open(f'/home/coen/Remote/Data/Wayback/wsj/articles/{timestamp}.pkl', 'wb') as f:
+            #     with open(f'/home/coen/Remote/Data/Wayback/nytimes/articles/{timestamp}.pkl', 'wb') as f:
             #         pickle.dump(art_dump, f)
 
         except AttributeError:
-            with open(f'/home/coen/Remote/Data/Wayback/wsj/raw/{timestamp}.pkl', 'wb') as f:
+            with open(f'/home/coen/Remote/Data/Wayback/nytimes/raw/{timestamp}.pkl', 'wb') as f:
                 pickle.dump(html, f)
