260ec834dbc16b6b1c0a6f14c64823b384841273
soybison <coeneedell@gmail.com>
2022-05-05 15:43:54 -0400

    update

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ config.read(os.path.join(HERE, 'config.ini'))
 
 def get_request(timestamp, intervals):
     try:
-        r = requests.get(f'https://web.archive.org/web/{timestamp}/https://www.nytimes.com/')
+        r = requests.get(f'https://web.archive.org/web/{timestamp}id_/https://www.nytimes.com/')
     except requests.exceptions.ConnectionError:
         time.sleep(60)
-        r = requests.get(f'https://web.archive.org/web/{timestamp}/https://www.nytimes.com/')
+        r = requests.get(f'https://web.archive.org/web/{timestamp}id_/https://www.nytimes.com/')
     except requests.exceptions.TooManyRedirects:
         time.sleep(60)
         new_timestamp_i = int(intervals.set_index('timestamp').index.get_loc(timestamp)) + 1
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     scraper = NYTimesScraper()
     tqdm.write('Acquiring CDX data')
     yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    intervals = cdxer.get_intervals('www.nytimes.com', hrs=1, period_start=datetime.datetime(2020, 1, 1),
+    intervals = cdxer.get_intervals('www.nytimes.com', hrs=1, period_start=datetime.datetime(2018, 1, 1),
                                     period_end=yesterday)
 
     if not os.path.exists('/home/coen/Remote/Data/Wayback'):
