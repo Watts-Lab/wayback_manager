cb484c587c15b803375af57fa23319961f50f529
SoyBison <coeneedell@gmail.com>
2022-08-19 10:17:54 -0400

    made main a little easier to use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import configparser
 import time
-from scrapers.nytimes import NYTimesScraper
 from cdx import WaybackCDX, WAYBACK_FORMAT
 import datetime
 import pickle
@@ -11,14 +10,15 @@ import requests
 HERE = os.path.dirname(__file__)
 config = configparser.ConfigParser()
 config.read(os.path.join(HERE, 'config.ini'))
-
+URL = "www.pbs.org/newshour"
+PUBCODE = "pbsnewshour"
 
 def get_request(timestamp, intervals):
     try:
-        r = requests.get(f'https://web.archive.org/web/{timestamp}id_/https://www.nytimes.com/')
+        r = requests.get(f'https://web.archive.org/web/{timestamp}id_/https://{URL}')
     except requests.exceptions.ConnectionError:
         time.sleep(60)
-        r = requests.get(f'https://web.archive.org/web/{timestamp}id_/https://www.nytimes.com/')
+        r = requests.get(f'https://web.archive.org/web/{timestamp}id_/https://{URL}')
     except requests.exceptions.TooManyRedirects:
         time.sleep(60)
         new_timestamp_i = int(intervals.set_index('timestamp').index.get_loc(timestamp)) + 1
@@ -29,27 +29,28 @@ def get_request(timestamp, intervals):
 
 if __name__ == '__main__':
     cdxer = WaybackCDX()
-    scraper = NYTimesScraper()
     tqdm.write('Acquiring CDX data')
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=2)
-    intervals = cdxer.get_intervals('www.nytimes.com', hrs=1, period_start=datetime.datetime(2018, 1, 1),
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=3)
+    intervals = cdxer.get_intervals(URL, hrs=1, period_start=datetime.datetime(2018, 1, 1),
                                     period_end=yesterday)
 
-    if not os.path.exists('/home/coen/Remote/Data/Wayback'):
-        os.mkdir('/home/coen/Remote/Data/Wayback')
-    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes'):
-        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes')
-    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes/raw'):
-        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes/raw')
-    if not os.path.exists('/home/coen/Remote/Data/Wayback/nytimes/articles'):
-        os.mkdir('/home/coen/Remote/Data/Wayback/nytimes/articles')
+    if not os.path.exists('/home/coen/Public/Wayback'):
+        os.mkdir('/home/coen/Public/Wayback')
+    if not os.path.exists(f'/home/coen/Public/Wayback/{pubcode}'):
+        os.mkdir(f'/home/coen/Public/Wayback/{pubcode}')
+    if not os.path.exists(f'/home/coen/Public/Wayback/{pubcode}/raw'):
+        os.mkdir(f'/home/coen/Public/Wayback/{pubcode}/raw')
+    if not os.path.exists(f'/home/coen/Public/Wayback/{pubcode}/articles'):
+        os.mkdir(f'/home/coen/Public/Wayback/{pubcode}/articles')
+    if not os.path.exists(f'/home/coen/Public/Wayback/{pubcode}/parsed'):
+        os.mkdir(f'/home/coen/Public/Wayback/{pubcode}/parsed')
 
     tqdm.write('Downloading data...')
 
     for timestamp in tqdm(intervals['timestamp'][intervals['is_target']]):
-        if os.path.exists(f'/home/coen/Remote/Data/Wayback/nytimes/raw/{timestamp}.pkl'):
+        if os.path.exists(f'/home/coen/Public/Wayback/{pubcode}/raw/{timestamp}.pkl'):
             continue
         r = get_request(timestamp, intervals)
         html = r.text
-        with open(f'/home/coen/Remote/Data/Wayback/nytimes/raw/{timestamp}.pkl', 'wb') as f:
+        with open(f'/home/coen/Public/Wayback/{pubcode}/raw/{timestamp}.pkl', 'wb') as f:
             pickle.dump(html, f)
