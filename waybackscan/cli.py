5316edfcdebebad5da81b16119683ee895be2212
soybison <coenn@seas.upenn.edu>
2022-12-14 12:32:37 -0500

    added -p support for abnormal urls

--- waybackscan/cli.py
+++ waybackscan/cli.py
@@ -7,6 +7,12 @@ from tzlocal import get_localzone
 import dateparser
 import sys
 
+URL_EXCEPTIONS = {
+        "abcnews": "www.abcnews.go.com",
+        "npr": "www.npr.org",
+        "pbs": "www.pbs.org/newshour",
+        "bbc": "www.bbc.com/news"
+        }
 HERE = get_localzone()
 
 
@@ -49,11 +55,18 @@ class IntervalParse(argparse.Action):
             out.append(round((datetime.now().replace(tzinfo=tartz) - tardate).total_seconds() / 3600))
         setattr(namespace, self.dest, out[0])
 
+class PublisherParse(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values in URL_EXCEPTIONS:
+            setattr(namespace, 'url', URL_EXCEPTIONS[values])
+        else:
+            setattr(namespace, 'url', f'www.{values}.com')
+
 
 def cli():
     parser = argparse.ArgumentParser()
     targetgrp = parser.add_mutually_exclusive_group(required=True)
-    targetgrp.add_argument("-p", "--publisher", type=str)
+    targetgrp.add_argument("-p", "--publisher", type=str, action=PublisherParse)
     targetgrp.add_argument("-u", "--url", type=str)
     dategrp = parser.add_argument_group()
     dategrp.add_argument("-s", "--start", type=str, action=DateParse)
@@ -65,13 +78,8 @@ def cli():
     parser.add_argument("-f", "--fail_ok", action='store_false')
 
     args = parser.parse_args()
-
-    url = None
-    if args.publisher:
-        url = f"www.{args.publisher}.com"
-        publisher = args.publisher
-    elif args.url:
-        url = args.url
+    
+    url = args.url
     cdxer = WaybackCDX()
 
     if args.interval:
